335c134306266098a66590b3ea0d7bbb4b56d172
sgx <sergio.gonzalez@soologic.com>
2025-03-25 14:52:55 +0100

    fix: api change for the backend url

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || 'https://api.miapp.com';
+const API_URL = process.env.NEXT_PUBLIC_API_URL || 'https://api.nucleav.com';
 
 export const getUsers = async () => {
   const response = await axios.get(`${API_URL}/users`);
