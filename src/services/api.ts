f730f94914570f0c0dcd92806a8e4e9a9d4cd4c0
Sergio Gonzalez <sergioglez997@gmail.com>
2025-04-01 08:28:23 +0200

    chore: adapt the project to the production or development in front

--- src/services/api.ts
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const API_URL = process.env.NEXT_PUBLIC_API_URL || 'https://api.nucleav.com';
-
-export const getUsers = async () => {
-  const response = await axios.get(`${API_URL}/users`);
-  return response.data;
-};
